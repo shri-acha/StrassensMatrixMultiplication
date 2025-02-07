97309469c06d1ca581a009bfb59639eb5608eb94
Apple Hunter <nerishcodes@gmail.com>
2025-02-07 19:19:51 +0545

    Removed unnecessary variables: row and column and replaced them with n
    Added comments for readability in matrix_split

--- matrix_create.go
+++ matrix_create.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
-
 func matrix_create(n int) (matrix [][]int) {
 	matrix = make([][]int, n) // Create n rows, columns not allocated
+	counter := 1
 	for i := range matrix {
 		matrix[i] = make([]int, n) //Iterate over n rows and create n columns per row
 	}
 
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			fmt.Scanf("%d", &matrix[i][j])
+			matrix[i][j] = counter
+			counter++
 		}
 	}
 
